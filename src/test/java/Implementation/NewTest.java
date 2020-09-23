fd462e9e29b65b6437466b7698c05f0ae69631ec
manwant <appiumg@gmail.com>
2020-09-23 07:44:34 -0700

    Final commit from Appiumtest

---
+++ src/test/java/Implementation/NewTest.java
@@ -0,0 +1,15 @@
+package Implementation;
+
+import org.openqa.selenium.By;
+
+import Capabilities.Desiredcaps;
+import Helpers.AsyncCalls;
+
+public class NewTest{
+
+	public static void main(String[] args)  {
+		
+
+	}
+
+}
